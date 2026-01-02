ebe1c264ccbb732a9e4297d97b24a89bd452a1dc
nsdHso <nechiforelsamuel@gmail.com>
2026-01-02 12:48:48 +0200

    feat: 🎸 use int and instantiated

--- examples/ch02/fig02_01.cpp
+++ examples/ch02/fig02_01.cpp
@@ -1,10 +1,12 @@
 // fig02_01.cpp
 // Text-printing program.
 #include <iostream> // enables program to output data to the screen
+#include <ostream>
 
 // function main begins program execution
 int main() {
-   std::cout << "Welcome to C++!\n"; // display message
+   std::printf("Wellcome in ");
+   std::printf("Wellcome in ");
 
    return 0; // indicate that program ended successfully
 } // end function main
